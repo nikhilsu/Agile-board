d887b90bd34733ee823deca5c45258292f23f4a9
nikhil sulegaon <nikhilsulegaon@gmail.com>
2017-12-05 23:57:35 -0700

    Revert "Add Project changes"
    This reverts commit eb8035864e649e40657e3ea888463559f7ee2c44.

--- src/main/java/com/prorg/service/ProjectService.java
+++
@@ -1,9 +0,0 @@
-package main.java.com.prorg.service;
-
-import com.prorg.model.User;
-
-import java.util.List;
-
-public interface ProjectService {
-    boolean createProject(String title, String description, String createdBy);
-}
--- src/main/webapp/WEB-INF/views/header.jsp
+++ src/main/webapp/WEB-INF/views/header.jsp
@@ -1,4 +1,3 @@
 <a href="/">Home</a>
 <a href="/login">Login</a>
 <a href="/register">Register</a>
-<a href="/addProject">Add Project</a>
