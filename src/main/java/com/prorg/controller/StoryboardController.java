82feeba9469c34cd05381624a25c9bb7a9f6d3c4
nikhil sulegaon <nikhilsulegaon@gmail.com>
2017-12-12 01:55:29 -0700

    Implemented a LoginInterceptor that ensures that a user is logged for all project related tasks.

--- src/main/java/com/prorg/controller/StoryboardController.java
+++ src/main/java/com/prorg/controller/StoryboardController.java
@@ -30,32 +30,27 @@ public class StoryboardController {
     }
 
     @RequestMapping(method = RequestMethod.GET)
-    public String showAddStoryboardForm(HttpSession session) {
-        if (isUserLoggedIn(session))
-            return Constants.RedirectPage.STORY_BOARD_FORM;
-        else
-            return Constants.RedirectPage.INDEX;
+    public String showAddStoryboardForm() {
+        return Constants.RedirectPage.STORY_BOARD_FORM;
     }
 
     @RequestMapping(method = RequestMethod.POST)
     public String addStoryboard(HttpServletRequest request, HttpSession session, Model model) throws Exception {
         String title = request.getParameter("title");
         String description = request.getParameter("description");
-        if (isUserLoggedIn(session)) {
-            int createdById = (int) session.getAttribute(Constants.SessionKeys.LOGGED_IN_USER);
-            Response response = userService.getUserById(createdById);
-            // TODO: Add of check for response failure.
-            User createdByUser = (User) response.data();
-            Response save = storyboardService.createStoryboard(title, description, createdByUser);
-            model.addAttribute(Constants.ModelAttributes.MESSAGE, save.isSuccessful() ? "Success" : "Failed");
-        }
+        int createdById = (int) session.getAttribute(Constants.SessionKeys.LOGGED_IN_USER);
+        Response response = userService.getUserById(createdById);
+        // TODO: Add of check for response failure.
+        User createdByUser = (User) response.data();
+        Response save = storyboardService.createStoryboard(title, description, createdByUser);
+        model.addAttribute(Constants.ModelAttributes.MESSAGE, save.isSuccessful() ? "Success" : "Failed");
         return Constants.RedirectPage.INDEX;
     }
 
     @RequestMapping(value = Constants.Route.UPDATE_USERS_OF_STORYBOARD, method = RequestMethod.POST)
     public String updateUsersWhoCanAccessStoryboard(HttpServletRequest request, @PathVariable("id") int storyboardId, Model model) throws Exception {
         ArrayList<User> usersWhoHaveAccess = new ArrayList<>();
-        for(String userId : request.getParameterValues("usersWhoHaveAccess")) {
+        for (String userId : request.getParameterValues("usersWhoHaveAccess")) {
             Response getUser = userService.getUserById(Integer.valueOf(userId));
             if (getUser.isSuccessful()) {
                 User user = (User) getUser.data();
@@ -66,9 +61,5 @@ public class StoryboardController {
 
         model.addAttribute(Constants.ModelAttributes.MESSAGE, updateAssignedUsersOfCard.isSuccessful() ? "Success" : "Failed");
         return Constants.RedirectPage.INDEX;
-    }
-
-    private boolean isUserLoggedIn(HttpSession session) {
-        return session.getAttribute(Constants.SessionKeys.LOGGED_IN_USER) != null;
     }
 }
---
+++ src/main/java/com/prorg/interceptor/LoginInterceptor.java
@@ -0,0 +1,23 @@
+package com.prorg.interceptor;
+
+import com.prorg.helper.Constants;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@Component
+public class LoginInterceptor extends HandlerInterceptorAdapter {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws IOException {
+        HttpSession session = request.getSession(false);
+        if(session == null || session.getAttribute(Constants.SessionKeys.LOGGED_IN_USER) == null) {
+            response.sendRedirect(Constants.Route.LOGIN);
+            return false;
+        }
+        return true;
+    }
+}
